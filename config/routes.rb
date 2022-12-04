08ee85b9b53d60a1405f394fa313463e4cbd9a44
Shivani Shilpi <shivanishilpi2120@gmail.com>
2022-12-05 01:50:06 +0530

    Tool controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
      end
   end
   root 'home#index'
+  get 'tool/index'  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
